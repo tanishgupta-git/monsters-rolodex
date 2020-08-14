9c5aa1e4a2830444330a8cd4f0f3d88af0313fbf
tanishgupta-git <tanishgupta8181@gmail.com>
2020-08-14 22:06:34 +0530

    Updates

---
+++ precache-manifest.6e1310da8c4317562a0f95edf377c2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985d5773bb5065d53df96838bf53454a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "27a678cba02ea4ff6afb",
+    "url": "/monsters-rolodex/static/css/main.b92b52da.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a678cba02ea4ff6afb",
+    "url": "/monsters-rolodex/static/js/main.cc6e7544.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
